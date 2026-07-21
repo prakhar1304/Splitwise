4be00610869eba27d0485eaccdc9009d18aa5a64
PRAKHAR MADHARIA <155750280+prakhar1304@users.noreply.github.com>
2026-07-21 22:30:18 +0530

    Add /ping endpoint that responds with pong

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,6 +13,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
 app.use("/api/users", userRouter);
 app.use("/api/groups", groupRouter);
 // Mount analytics BEFORE expenses router so /analytics is not matched by /:id
